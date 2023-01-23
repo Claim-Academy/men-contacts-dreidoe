8d1bb43d51d743d0b27646b17b84b6a95e1dc635
Aundrei Bryant <drei97@gmail.com>
2023-01-23 11:08:57 -0600

    mongoose validation added

--- app/contacts/Contacts.js
+++ app/contacts/Contacts.js
@@ -1,12 +1,14 @@
 import { Schema, model } from "mongoose";
+// scehma for the contacts
+// schema is a blueprint for the data
 
 export default model(
   "Contacts",
   new Schema({
-    fullName: String,
-    userName: String,
-    phrase: String,
-    avatar: String,
-    id: String,
+    fullName: { type: String, required: true },
+    userName: { type: String, required: true },
+    phrase: { type: String, required: false },
+    avatar: { type: String, required: false },
+    id: { type: String, required: true },
   })
 );
