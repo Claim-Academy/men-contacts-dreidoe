52e783c6115e411b794184473d97b41968ea6012
Aundrei Bryant <drei97@gmail.com>
2023-01-23 11:35:44 -0600

    validation added

--- app/contacts/Contacts.js
+++ app/contacts/Contacts.js
@@ -6,7 +6,13 @@ export default model(
   "Contacts",
   new Schema({
     fullName: { type: String, required: true },
-    userName: { type: String, required: true },
+    userName: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: [true, "Username already exists"],
+      minlength: [3, "Username must be at least 3 characters long"],
+      maxlength: [20, "Username must be at most 20 characters long"],
+    },
     phrase: { type: String, required: false },
     avatar: { type: String, required: false },
     id: { type: String, required: true },
